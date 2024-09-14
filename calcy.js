5d891dafcbed5087c41d0ee202e1195e4b7794c1
rahul <rahukumarbce0127@gmail.com>
2024-09-15 02:38:20 +0530

    calcy

---
+++ calcy.js
@@ -0,0 +1,29 @@
+const output = document.querySelector('.result')
+
+const buttons = document.querySelectorAll('button')
+
+let str = '';
+
+buttons.forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        console.log(e.target.textContent.trim())
+        if(e.target.textContent.trim() === 'AC'){
+            str = '';
+            output.value = str;
+        }else if (e.target.textContent.trim() === 'DEL'){
+            str = str.substring(0, str.length - 1)
+            output.value = str;
+        }else if (e.target.textContent.trim() === '='){
+            try{
+                str = eval(str);
+            output.value = str;
+            } catch(error){
+                output.value = 'Error'
+            }
+        }
+        else {
+            str += e.target.textContent.trim();
+            output.value = str;
+        }
+    })
+})
